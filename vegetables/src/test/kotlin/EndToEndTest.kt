be372fa729c926c29dca83d581d65059dc4c302b
Marco Fracassi <fracassi.marco@gmail.com>
2019-12-13 10:57:38 +0100

    Initial commit

---
+++ vegetables/src/test/kotlin/EndToEndTest.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class EndToEndTest {
+
+    @Test
+    fun `add a vegetable`() {
+        assertThat(true).isEqualTo(true)
+    }
+}
